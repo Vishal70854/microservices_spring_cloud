a3ab885c3895db0351ac1f7f6dfa2f8dd513c77c
Vishal Kumar Singh <vishalkumar70854@gmail.com>
2025-09-05 11:07:40 +0530

    implemented load balancer which talks to different instances of currency-exchange from currency-conversion using Feign client

--- currency-conversion-service/src/main/java/com/in28minutes/microservices/currency_conversion_service/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currency_conversion_service/CurrencyExchangeProxy.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "currency-exchange", url = "localhost:8000")
+//@FeignClient(value = "currency-exchange", url = "localhost:8000") // this will only connect to currency-exchange service using the port 8000
+@FeignClient(value = "currency-exchange")   // only using currency-exchange as app name will talk to all instances of currency-exchange using load balancing factor
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
