e5be80a9ff017e8edabedd70426add9656d2f1f2
Vishal Kumar Singh <vishalkumar70854@gmail.com>
2025-09-12 11:20:47 +0530

    implemented distributed tracing server i.e zipkin in currency-exchange-service (using micrometer, opentelemetry)

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservices.currency_exchange_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private Environment environment;
 
@@ -21,6 +25,8 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to){
+
+        logger.info("retrieveExchangeValue called with {} to {}",from, to)  ;
 
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if(currencyExchange == null){
